cdd10f8a29471e968500c38f6ff4b9cc19719bda
Adrien Pétremann <hello@grena.fr>
2018-10-09 23:23:52 +0200

    Add type hint on LayerOrchestrator

--- src/Odin/Orchestrator/LayerOrchestrator.php
+++ src/Odin/Orchestrator/LayerOrchestrator.php
@@ -12,7 +12,7 @@ class LayerOrchestrator
     private $width;
     private $height;
 
-    public function initBaseLayer(int $width, int $height, string $hexColor = '#000', int $alpha = 0)
+    public function initBaseLayer(int $width, int $height, string $hexColor = '#000', int $alpha = 0): void
     {
         $this->width = $width;
         $this->height = $height;
@@ -20,7 +20,7 @@ class LayerOrchestrator
         $this->setBackgroundColor($hexColor, $alpha);
     }
 
-    public function setBaseLayer($image)
+    public function setBaseLayer($image): void
     {
         imagesavealpha($image, true);
         $this->image = $image;
@@ -29,7 +29,7 @@ class LayerOrchestrator
         $this->height = imagesy($image);
     }
 
-    public function setBackgroundColor($hexColor = '#000', $alpha = 0)
+    public function setBackgroundColor(string $hexColor = '#000', int $alpha = 0): void
     {
         list($r, $g, $b) = ColorHelper::hexToRgb($hexColor);
         $this->image = imagecreatetruecolor($this->width, $this->height);
@@ -38,7 +38,7 @@ class LayerOrchestrator
         imagefill($this->image, 0, 0, $backgroundColor);
     }
 
-    public function addLayer($layer, int $dst_x = 0, int $dst_y = 0)
+    public function addLayer($layer, int $dst_x = 0, int $dst_y = 0): void
     {
         $xSize = imagesx($layer);
         $ySize = imagesy($layer);
