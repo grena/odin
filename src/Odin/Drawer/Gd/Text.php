55e76654ab2d444f94bedd8daff1a39c1cbf3840
Adrien Pétremann <hello@grena.fr>
2018-10-12 13:10:09 +0200

    Fix path of Font to be used with CLI

--- src/Odin/Drawer/Gd/Text.php
+++ src/Odin/Drawer/Gd/Text.php
@@ -8,7 +8,7 @@ class Text
 {
     public static function write($image, $text, $x, $y)
     {
-        $font_file = './IBMPlexMono-Regular.ttf';
+        $font_file = __DIR__ . '/../../../IBMPlexMono-Regular.ttf';
         $white = imagecolorallocate($image, 255, 255, 255);
         imagefttext($image, 8, 0, $x, $y, $white, $font_file, $text);
     }
